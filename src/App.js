8c7e0c37e6cbbf908a25f2a3523ff91fb04d9c33
aayush1408 <aayushjaiswal0114@gmail.com>
2019-01-11 19:26:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      parameters: []
+    }
+  }
+  componentDidMount() {
+    axios('http://localhost:5000/get-data').then(({ data }) => {
+      this.setState({ parameters: [...data] })
+      console.log(this.state.parameters);
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Dashboard</h1>
         </header>
       </div>
     );
